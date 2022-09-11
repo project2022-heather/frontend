5ce937a3ff986a4ff8f27274747373895f251d44
na2na-p <na2na@na2na.dev>
2022-09-11 22:57:04 +0900

    Update ESLint Settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,13 +18,24 @@ module.exports = {
   'plugins': [
     'react',
     '@typescript-eslint',
+    '@emotion',
   ],
   'rules': {
     'new-cap': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/display-name': 'off', // ファイル名とコンポーネント名を一致させることで解決させるので不要
+    'react/react-in-jsx-scope': 'off',
+    'react/no-unknown-property': [
+      'error',
+      {ignore: ['css']},
+    ],
+    'require-jsdoc': 'off',
+    'react/display-name': 'off',
     'max-len': 'off',
     'no-tabs': 'off',
+    '@emotion/no-vanilla': 'error',
+    '@emotion/import-from-emotion': 'error',
+    '@emotion/styled-import': 'error',
   },
   'settings': {
     'react': {
